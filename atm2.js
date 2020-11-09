2fce961bb77172ac191faaff42d9d54eff106253
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2020-11-09 11:45:48 -0600

    Nearly there

--- atm2.js
+++ atm2.js
@@ -8,15 +8,16 @@ atm.createAccount (1013, 1900);
 function Atm() {
 
     this.accounts = []; //empty array for acct
-    this.currentAccount = null; //set new account ammount to empty
-
+    this.currentAccount = null; //set new account to empty
+    //creates new account with PIN and balance
     this.createAccount = function(pin, balance) {
+        //pushes new accounts into constructor obj array
         let newAccount = new Account(pin, balance);
         this.accounts.push(newAccount);
         this.currentAccount = newAccount;
         return newAccount;
     };
-
+    //runs entered PIN through loo to see if it exists
     this.getAccount = function (pin) {
         for (let i = 0; i <this.accounts.length; i++){
             if (this.accounts[i].pin === pin){
@@ -30,23 +31,23 @@ function Atm() {
     
     this.configuration = {};
 }
-
+//Account constucotr for user info and the math
 function Account(pin, balance) {
     this.pin = pin;
     this.balance = balance;
     
-
+//check balance of current account
     this.getBalance = function() {
         return(this.balance);
     }
-
+//removes amount
     this.withdrawl = function(amount) {
         console.log( typeof balance);
         this.balance -= amount;
         console.log (typeof balance);
 
     }
-
+//adds amount
     this.deposit = function(amount) {
         console.log(amount);
         this.balance = Number(this.balance)+ amount;
@@ -54,7 +55,7 @@ function Account(pin, balance) {
 
     }
 }
-
+//on click function for returning to the log in page
 function returnToLogIn() {
 
     document.getElementById("intro").style.display= "block";
@@ -62,12 +63,12 @@ function returnToLogIn() {
     document.getElementById("menu").style.display= "none";
 
 }
-
+//shows current balance when button is clicked
 function showBalance() {
 
     document.getElementById("showBalance").value=atm.currentAccount.balance;
 }
-
+//checks for existing PIN, alerts and askes to create new account if not
 function confirmAccount() {
 
     let pin = parseInt(document.getElementById("enterPin").value); 
@@ -82,6 +83,7 @@ function confirmAccount() {
     }
     return;
 }
+//creates new account and pushes it into constructor array
 function newAccount() {
 
     let pin = parseInt(document.getElementById("enterPin").value);
@@ -96,12 +98,12 @@ function newAccount() {
  function displayMenu (){
     //hides PIN entry menu
     document.getElementById("intro").style.display= "none";
-    //shows menu
+    //shows menu page
     document.getElementById("menu").style.display= "block";
  }
  
 function atmMath() {
-
+//gets the input from the HTML
     let withdrawl = document.getElementById("withdrawl").value;
     let deposit = document.getElementById("deposit").value;
     let showBalance = document.getElementById("showBalance").value;
